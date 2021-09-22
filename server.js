c0c7a11952a6ad3c1bccc50521f3469030603d35
Zupirio <zupirio@gmail.com>
2021-09-22 11:15:38 +0200

    add login POST route

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 
-const mockUserData=[
-	{name: 'Mark'},
-	{name: 'Jill'},
+const mockUserData = [
+	{ name: 'Mark' },
+	{ name: 'Jill' },
 ]
 
-app.get('/users', function(req, res){
+app.get('/users', function (req, res) {
 	res.json({
 		success: true,
 		message: 'successfully got users. Nice!',
@@ -15,7 +18,7 @@ app.get('/users', function(req, res){
 	})
 })
 
-app.get('/users/:id', function(req, res){
+app.get('/users/:id', function (req, res) {
 	console.log(req.params.id);
 	res.json({
 		success: true,
@@ -24,7 +27,29 @@ app.get('/users/:id', function(req, res){
 	})
 })
 
+app.post('/login', function (req, res) {
+	const username = req.body.params.username;
+	const password = req.body.params.password;
 
-app.listen(8000,function(){
+	const mockUsername = "billyTheKid";
+	const mockPassword = "superSecret";
+
+	if (username === mockUsername && password === mockPassword) {
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	}
+	else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
+})
+
+
+app.listen(8000, function () {
 	console.log("server is running")
 })
